6fba12694eff37d1f62d0f3feb483ad524b7f004
MasonDG <gloverm3@uni.coventry.ac.uk>
2016-12-02 16:42:26 +0000

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -2,23 +2,12 @@
 #existing list and based on the length of the list and a random integer,
 #if the value pulled matches the current state of the list, it will
 #write out that specific entry into a new list then repeat until all previous entries are written into the new list.
-From random import randint
+import random
 def array(x):
-    #makes a integer of the list's length 
-    length = len(x) - 1
-    #for loop for every entry from the passed list
-    for i in range(length, 0, -1):
-        #value variable can be anything from 0 to the length of the list being passed..
+    for i in range(0,len(x)):
         value = random.randint(0,i)
-        #If the value matches the integer position no switch would occur
-        if value == i:
-            pass
-        #Rearranges the value selected to the first available position.
-        x[value],x[i] = x[i], x[value]
-    #prints the shuffled list
-    return x
-
-
+        x[value],x[i] = x[i],x[value]
+    return(x)
 #The list to be used
 myArray = [5,3,8,6,1,9,2,7]
 
